0dc991081fe6cd36968b438c3274785c97934297
andrraa <andraramadanp@outlook.com>
2024-07-20 21:53:31 +0700

    chore: Install sweetalert2 as toast notification

---
+++ src/app/services/alert/alert.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertService {
+  showToast(options: { icon: SweetAlertIcon; title: string }): void {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      customClass: {
+        popup: 'alert-popup',
+      },
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      },
+    });
+
+    Toast.fire({
+      icon: options.icon,
+      title: options.title,
+      customClass: {
+        popup: 'alert-popup',
+      },
+    });
+  }
+}
